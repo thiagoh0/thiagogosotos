8ce7dc95825ac2c20c71a9487ed5010e2dd9a7e5
thiagoh0 <161467089+thiagoh0@users.noreply.github.com>
2024-10-22 09:27:49 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'politica',
+    'lula ou bolsonaro?',
+    'bolsonaro'
+)
+criaCartao(
+    'Carro',
+    'mustang ou ferrari?',
+    'ferrari'
+)
+criaCartao(
+    'paises',
+    'estados unidos ou canada?',
+    'canada'
+)
+criaCartao(
+    'Computador',
+    'O que é um vírus?',
+    'Em informática, um vírus de computador é um software malicioso que é desenvolvido por programadores geralmente inescrupulosos'
+)
